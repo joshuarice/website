725a7327d530c3b3e54d9f330b6019a8ff73ee14
jrice <jrice@streetleague.io>
2025-02-12 21:45:24 -0700

    fix links

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -5,22 +5,31 @@ export default function SocialIcons() {
     return (
         <ul className="flex gap-4">
             <li>
-                <Link href="https://www.linkedin.com/in/joshuarice1/">
+                <Link
+                    href="https://www.linkedin.com/in/joshuazrice/"
+                    target="_blank"
+                >
                     <Linkedin className="h-6 w-6 text-personal hover:text-personal/80" />
                 </Link>
             </li>
             <li>
-                <Link href="https://www.instagram.com/joshuarice1/">
+                <Link
+                    href="https://www.instagram.com/jricefpv/"
+                    target="_blank"
+                >
                     <Instagram className="h-6 w-6 text-personal hover:text-personal/80" />
                 </Link>
             </li>
             <li>
-                <Link href="https://github.com/joshuarice1">
+                <Link href="https://github.com/joshuarice" target="_blank">
                     <Github className="h-6 w-6 text-personal hover:text-personal/80" />
                 </Link>
             </li>
             <li>
-                <Link href="https://www.youtube.com/@joshuarice1">
+                <Link
+                    href="https://www.youtube.com/channel/UCpYYMr7mZT370TBwSOZuqBA"
+                    target="_blank"
+                >
                     <Youtube className="h-6 w-6 text-personal hover:text-personal/80" />
                 </Link>
             </li>
